933f3d1fd5b5c1aa270b4dcf4438af231c0e2d84
Enes Yöndeş <91937666+enesyondes@users.noreply.github.com>
2023-01-28 09:59:05 +0300

    code.py

---
+++ code.py
@@ -0,0 +1,24 @@
+
+li=[]
+def flatten(n):
+     for i in n:
+         if isinstance(i,list):#Kontrol ediyor liste mi diye
+          flatten(i)
+         else:
+          li.append(i)#Eğer liste değilse boş olan li elemanına ekliyor
+
+
+liste=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+flatten(liste)
+print(li)
+
+
+def reverse_list(n):
+ n = n[::-1]
+ n = [i[::-1] for i in n]
+ return n
+
+liste1=[[1, 2], [3, 4], [5, 6, 7]]
+
+
+print(reverse_list(liste1))
